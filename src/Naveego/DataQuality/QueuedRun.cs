ab99722fdea8b58aba2498207d6765a36f83841d
Derek Smith <derek.smith@naveego.com>
2016-10-13 10:41:11 -0400

    Added QueuedRun

---
+++ src/Naveego/DataQuality/QueuedRun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Naveego.DataQuality
+{
+    public class QueuedRun
+    {
+
+        [JsonProperty("check_id")]
+        public Guid CheckId { get; set; }
+
+        [JsonProperty("rule_id")]
+        public Guid RuleId { get; set; }
+
+        [JsonProperty("connection_id")]
+        public Guid ConnectionId { get; set; }
+
+        [JsonProperty("client_id")]
+        public Guid SyncClientId { get; set; }
+
+        [JsonProperty("queued_on")]
+        public DateTime QueuedOn { get; set; }
+
+        [JsonProperty("queued_by")]
+        public string QueuedBy { get; set; }
+
+
+    }
+}
